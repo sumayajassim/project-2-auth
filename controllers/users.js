fdbc14c4c9b8162bdac5a77afa168e84c8397010
TaylorDarneille <tmdarneille@gmail.com>
2022-02-24 13:02:42 -0800

    part 2 complete

--- controllers/users.js
+++ controllers/users.js
@@ -12,4 +12,29 @@ router.post('/', async (req, res)=>{
     res.redirect('/')
 })
 
+router.get('/login', (req, res)=>{
+    res.render('users/login.ejs')
+})
+
+router.post('/login', async (req, res)=>{
+    const user = await db.user.findOne({where: {email: req.body.email}})
+    if(!user){
+        console.log('user not found')
+        res.render('users/login', { error: "Invalid email/password" })
+    } else if(user.password !== req.body.password) {
+        console.log('password incorrect')
+        res.render('users/login', { error: "Invalid email/password" })
+    } else {
+        console.log('logging in the user!!!')
+        res.cookie('userId', user.id)
+        res.redirect('/')
+    }
+})
+
+router.get('/logout', (req, res)=>{
+    console.log('logging out')
+    res.clearCookie('userId')
+    res.redirect('/')
+})
+
 module.exports = router
