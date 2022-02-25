08abbeeb5ba1dee0d89942f7bf3cecf5248df1dd
TaylorDarneille <tmdarneille@gmail.com>
2022-02-24 16:42:46 -0800

    part 3 complete

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const app = express()
 const ejsLayouts = require('express-ejs-layouts')
+const cookieParser = require('cookie-parser')
+const db = require('./models')
 
 // MIDDLEWARE
 app.set('view engine', 'ejs')
 app.use(ejsLayouts)
+app.use(cookieParser())
 app.use(express.urlencoded({extended: false}))
+
+// AUTHENTICATION MIDDLEWARE
+app.use(async (req, res, next)=>{
+    if(req.cookies.userId) {
+        const user = await db.user.findByPk(req.cookies.userId)
+        res.locals.user = user
+    }
+    next()
+})
 
 // CONTROLLERS
 app.use('/users', require('./controllers/users'))
